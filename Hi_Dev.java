93fa79b721ffcf78ef9e2ab05b50264485547377
pat_mic <pat_mic@hotmail.com>
2023-11-13 11:52:48 -0500

    C1: hola java

---
+++ Hi_Dev.java
@@ -0,0 +1,5 @@
+public class Hi_Dev{
+    public static void main(String[] args) {
+        System.out.println("Hi developers...");
+    }
+}
---
+++ Hi_Patmic.java
@@ -0,0 +1,5 @@
+public class Hi_Patmic {
+    public static void main(String[] args) {
+        System.out.println("hi pat_mic");
+    } 
+}
